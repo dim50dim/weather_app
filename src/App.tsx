52ae6bfb89c91b196f8c3df992aa6454a81e8b4c
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-31 10:02:57 +0200

    styled app component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-
+import axios from "axios"
 function App() {
-
+const url =  `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=f49fe35ac16f22b774068f82b5cecaa7`
 
   return (
     <>
-      <div className="font-bold text-5xl bg-red-400">TAILWINDCSS</div>
+       <div className="app">
+           <div className="container">
+             <div className="top">
+                <div className="location">
+                  <p>Dallas</p>
+             </div>
+              <div className="temp">
+                   <h1>60F</h1>
+              </div>
+              <div className="description">
+                <p>Clouds</p>
+              </div>
+             <div className="bottom">
+              <div className="feels">
+                   <p>65F</p>
+              </div>
+              <div className="humidity">
+                <p>20%</p>
+              </div>
+              <div className="wind">
+                <p>12MPH</p>
+              </div>
+             </div>
+           </div>
+      </div> 
+      </div>
       
     </>
   )
 }
 
-export default App
+export default App;
